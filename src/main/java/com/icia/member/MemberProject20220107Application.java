aadf56199f73af83afe0acb3f108ce99b23db0b4
sunyoong <haert2010@naver.com>
2022-03-25 11:40:27 +0900

    220325

--- src/main/java/com/icia/member/MemberProject20220107Application.java
+++ src/main/java/com/icia/member/MemberProject20220107Application.java
@@ -1,13 +1,13 @@
 package com.icia.member;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MemberProject20220107Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MemberProject20220107Application.class, args);
-    }
-
-}
+//@SpringBootApplication
+//public class MemberProject20220107Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(MemberProject20220107Application.class, args);
+//    }
+//
+//}
